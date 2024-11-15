7f27b58cd51fd63b91a9d0eb0c5fee0d6ba6e6d0
Alw1n-oss <Bespalov565@gmail.com>
2024-11-15 18:59:45 +0300

    Create solution.py

---
+++ 9.3. Словарь/A/solution.py
@@ -0,0 +1,11 @@
+unknown_dict = dict()
+answer = []
+q = int(input())
+for _ in range(q):
+    query = input().split()
+    if query[0] == '1':
+        unknown_dict[query[1]] = query[2]
+    elif query[0] == '2':
+        temp = unknown_dict.get(query[1], '-1')
+        answer.append(temp)
+print('\n'.join(answer))
